c220d8411e51fca13cc323f1346a196be3b9034e
janasvidorge-hub <janasvidorge@gmail.com>
2026-08-30 17:59:30 +0530

    Update calculator program

--- calculator.py
+++ calculator.py
@@ -15,3 +15,5 @@ print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
 print("Division:", divide(10, 5))
+
+print("DevOps CI/CD demonstration")
